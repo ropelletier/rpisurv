653e61c94a98e90dfcf8b31a0f2bcd107ce0a964
ropelletier <88781432+ropelletier@users.noreply.github.com>
2024-02-01 08:27:23 -0500

    Create install_reqs.sh

---
+++ install_reqs.sh
@@ -0,0 +1,12 @@
+sudo apt install libvlc-bin=3.0.17.4-0+deb10u2+rpt1 vlc-data=3.0.17.4-0+deb10u2+rpt1
+sudo apt install vlc=3.0.17.4-0+deb10u2+rpt1 \
+vlc-bin=3.0.17.4-0+deb10u2+rpt1 \
+vlc-plugin-base=3.0.17.4-0+deb10u2+rpt1 \
+vlc-plugin-qt=3.0.17.4-0+deb10u2+rpt1 \
+vlc-plugin-video-output=3.0.17.4-0+deb10u2+rpt1 \
+vlc-l10n=3.0.17.4-0+deb10u2+rpt1 \
+vlc-plugin-notify=3.0.17.4-0+deb10u2+rpt1 \
+vlc-plugin-samba=3.0.17.4-0+deb10u2+rpt1 \
+vlc-plugin-skins2=3.0.17.4-0+deb10u2+rpt1 \
+vlc-plugin-video-splitter=3.0.17.4-0+deb10u2+rpt1 \
+vlc-plugin-visualization=3.0.17.4-0+deb10u2+rpt1 -y
