a006c71bb48626662d8bf194bf5aa2ff6fc27a5d
ropelletier <88781432+ropelletier@users.noreply.github.com>
2024-02-01 09:02:09 -0500

    Update install_reqs.sh

--- install_reqs.sh
+++ install_reqs.sh
@@ -5,11 +5,7 @@ sudo apt install -y vlc-plugin-base=3.0.17.4-0+deb10u2+rpt1 --no-install-recomme
 sudo apt install -y vlc-plugin-qt=3.0.17.4-0+deb10u2+rpt1 --no-install-recommends
 sudo apt install -y vlc-plugin-video-output=3.0.17.4-0+deb10u2+rpt1 --no-install-recommends
 
-sudo apt install -y vlc=3.0.17.4-0+deb10u2+rpt1 \
-vlc-bin=3.0.17.4-0+deb10u2+rpt1 \
-vlc-plugin-base=3.0.17.4-0+deb10u2+rpt1 \
-vlc-plugin-qt=3.0.17.4-0+deb10u2+rpt1 \
-vlc-plugin-video-output=3.0.17.4-0+deb10u2+rpt1 \
+sudo apt --allow-downgrades install -y vlc=3.0.17.4-0+deb10u2+rpt1 \
 vlc-l10n=3.0.17.4-0+deb10u2+rpt1 \
 vlc-plugin-notify=3.0.17.4-0+deb10u2+rpt1 \
 vlc-plugin-samba=3.0.17.4-0+deb10u2+rpt1 \
