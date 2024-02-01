ddefd97b93091d8136256ca42e9e59edc4376dbf
ropelletier <88781432+ropelletier@users.noreply.github.com>
2024-02-01 08:39:13 -0500

    Update install_reqs.sh

--- install_reqs.sh
+++ install_reqs.sh
@@ -1,5 +1,11 @@
-sudo apt install libvlc-bin=3.0.17.4-0+deb10u2+rpt1 vlc-data=3.0.17.4-0+deb10u2+rpt1
-sudo apt install vlc=3.0.17.4-0+deb10u2+rpt1 \
+sudo apt install -y libvlc-bin=3.0.17.4-0+deb10u2+rpt1 --no-install-recommends
+sudo apt install -y vlc-data=3.0.17.4-0+deb10u2+rpt1 --no-install-recommends
+sudo apt install -y vlc-bin=3.0.17.4-0+deb10u2+rpt1 --no-install-recommends
+sudo apt install -y vlc-plugin-base=3.0.17.4-0+deb10u2+rpt1 --no-install-recommends 
+sudo apt install -y vlc-plugin-qt=3.0.17.4-0+deb10u2+rpt1 --no-install-recommends
+sudo apt install -y vlc-plugin-video-output=3.0.17.4-0+deb10u2+rpt1 --no-install-recommends
+
+sudo apt install -y vlc=3.0.17.4-0+deb10u2+rpt1 \
 vlc-bin=3.0.17.4-0+deb10u2+rpt1 \
 vlc-plugin-base=3.0.17.4-0+deb10u2+rpt1 \
 vlc-plugin-qt=3.0.17.4-0+deb10u2+rpt1 \
@@ -9,4 +15,4 @@ vlc-plugin-notify=3.0.17.4-0+deb10u2+rpt1 \
 vlc-plugin-samba=3.0.17.4-0+deb10u2+rpt1 \
 vlc-plugin-skins2=3.0.17.4-0+deb10u2+rpt1 \
 vlc-plugin-video-splitter=3.0.17.4-0+deb10u2+rpt1 \
-vlc-plugin-visualization=3.0.17.4-0+deb10u2+rpt1 -y
+vlc-plugin-visualization=3.0.17.4-0+deb10u2+rpt1
